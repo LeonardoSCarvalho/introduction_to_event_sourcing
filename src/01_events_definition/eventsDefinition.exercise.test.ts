630af405d5920b05156fb9d2a4beba952c56e1c4
leonardo <leo.carvalholkp@gmail.com>
2025-02-17 19:36:26 -0300

    feat: 01 events definition

---
+++ src/01_events_definition/eventsDefinition.exercise.test.ts
@@ -0,0 +1,142 @@
+// 1. Define your events and entity here
+import { v4 as uuid } from 'uuid';
+
+export type Event<
+  EventType extends string = string,
+  EventData extends Record<string, unknown> = Record<string, unknown>,
+> = Readonly<{
+  type: Readonly<EventType>;
+  data: Readonly<EventData>;
+}>;
+
+// {
+//   type: 'ShoppingCartOpened';
+//   data: {
+//     shoppingCartId: string;
+//     clientId: string;
+//     openedAt: Date;
+//   };
+// }
+
+export interface ProductItem {
+  productId: string;
+  quantity: number;
+}
+
+export type PricedProductItem = ProductItem & {
+  unitPrice: number;
+};
+
+export type ShoppingCartOpened = Event<
+  'ShoppingCartOpened', // Carrinho de compras aberto
+  {
+    shoppingCartId: string;
+    clientId: string;
+    openedAt: Date;
+  }
+>;
+
+export type ProductItemAddedToShoppingCart = Event<
+  'ProductItemAddedToShoppingCart', // produto adicionado no carrinho de compra
+  {
+    shoppingCartId: string;
+    productItem: PricedProductItem;
+  }
+>;
+
+export type ProductItemRemovedFromShoppingCart = Event<
+  'ProductItemRemovedFromShoppingCart', // produto removido  no carrinho de compra
+  {
+    shoppingCartId: string;
+    productItem: PricedProductItem;
+  }
+>;
+
+export type ShoppingCartConfirmed = Event<
+  'ShoppingCartConfirmed', // carrinho de compra confirmado
+  {
+    shoppingCartId: string;
+    confirmedAt: Date;
+  }
+>;
+
+export type ShoppingCartCanceled = Event<
+  'ShoppingCartCanceled', // carrinho de compra cancelado
+  {
+    shoppingCartId: string;
+    canceledAt: Date;
+  }
+>;
+
+export type ShoppingCartEvent =
+  | ShoppingCartOpened
+  | ProductItemAddedToShoppingCart
+  | ProductItemRemovedFromShoppingCart
+  | ShoppingCartConfirmed
+  | ShoppingCartCanceled;
+
+enum ShoppingCartStatus {
+  Pending = 'Pending',
+  Confirmed = 'Confirmed',
+  Canceled = 'Canceled',
+}
+
+export type ShoppingCart = Readonly<{
+  id: string;
+  clientId: string;
+  status: ShoppingCartStatus;
+  productItems: PricedProductItem[];
+  openedAt: Date;
+  confirmedAt?: Date;
+  canceledAt?: Date;
+}>;
+
+describe('Events definition', () => {
+  it('all event types should be defined', () => {
+    const shoppingCartId = uuid();
+    const clientId = uuid();
+    const pairOfShoes: PricedProductItem = {
+      productId: uuid(),
+      quantity: 1,
+      unitPrice: 100,
+    };
+
+    const events: ShoppingCartEvent[] = [
+      // 2. Put your sample events here
+      {
+        type: 'ShoppingCartOpened',
+        data: {
+          shoppingCartId,
+          clientId,
+          openedAt: new Date(),
+        },
+      },
+      {
+        type: 'ProductItemAddedToShoppingCart',
+        data: {
+          shoppingCartId,
+          productItem: pairOfShoes,
+        },
+      },
+      {
+        type: 'ProductItemRemovedFromShoppingCart',
+        data: { shoppingCartId, productItem: pairOfShoes },
+      },
+      {
+        type: 'ShoppingCartConfirmed',
+        data: {
+          shoppingCartId,
+          confirmedAt: new Date(),
+        },
+      },
+      {
+        type: 'ShoppingCartCanceled',
+        data: {
+          shoppingCartId,
+          canceledAt: new Date(),
+        },
+      },
+    ];
+    expect(events.length).toBe(5);
+  });
+});
